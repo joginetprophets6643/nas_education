12d338912f66c43806d9129ab8a84c6df9fec03b
Naman <naman@gmail.com>
2021-12-23 16:55:40 +0530

    user-table: change

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,16 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('user_type')->nullable();
+            $table->string('identity_yourself')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('address')->nullable();
+            $table->string('mobile_no')->nullable();
+            $table->string('country')->nullable();
+            $table->string('state_id')->nullable();
+            $table->string('district_id')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
