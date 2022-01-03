ec20deee1186e0309323320cbbf200dd557e69d8
chankyvats <chanky629@gmail.com>
2022-01-03 15:49:31 +0530

    Tq Feedback

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -10,6 +10,10 @@ use App\Models\FeedbackData;
 use App\Models\PQNationaLevelFeedback;
 use App\Models\PQStateLevelFeedback;
 use App\Models\PQDistrictLevelFeedback;
+use App\Models\TQs;
+use App\Models\TqAnswerKey;
+use App\Models\TQQuestionMaster;
+use App\Models\FeedbackDataTq;
 use DB;
 class FeedbackController extends Controller
 {
@@ -21,7 +25,8 @@ class FeedbackController extends Controller
          * Date: 30/12/2021
          * Start Here
          *************************************************************/
-        ini_set('max_execution_time', '500');
+        ini_set('max_execution_time', '1500');
+        DB::table('feedback_data')->truncate();
         $pqData =  PQs::with(['NasExamDetails'=>function($q){
             $q->select('state_cd','district_cd','udise_sch_code');
         }])->get();
@@ -76,7 +81,7 @@ class FeedbackController extends Controller
         $newFeedbackData = array();
         if(count($FeedbackData)>0)
         {
-            DB::table('pq_district_level_feedback')->truncate();
+            DB::table('pq_district_level_feedback')->where('level','pq')->truncate();
             foreach($FeedbackData as $newLOData)
             {
                 $newFeedbackData['state_id'] = (int)$newLOData->state_id;
@@ -112,7 +117,7 @@ class FeedbackController extends Controller
         $newFeedbackDataS = array();
         if(count($FeedbackDataForState)>0)
         {
-            DB::table('pq_state_level_feedback')->truncate();
+            DB::table('pq_state_level_feedback')->where('level','pq')->truncate();
             foreach($FeedbackDataForState as $newLODataState)
             {
                 $newFeedbackDataS['state_id'] = (int)$newLODataState->state_id;
@@ -146,7 +151,7 @@ class FeedbackController extends Controller
         $newFeedbackDataNational = array();
         if(count($FeedbackDataNational)>0)
         {
-            DB::table('pq_national_level_feedback')->truncate();
+            DB::table('pq_national_level_feedback')->where('level','pq')->truncate();
             foreach($FeedbackDataNational as $newLODataNational)
             {
                 $newFeedbackDataNational['grade'] = (int)$newLODataNational->grade;
@@ -481,6 +486,354 @@ class FeedbackController extends Controller
                          ,'pq_q39' 
                          ,'pq_q40'
                        ];
+
+        return $arr[$key];
+    }
+
+    public function get_feedback_tq()
+    {   
+        ini_set('max_execution_time', '1500');
+        DB::table('feedback_data_tq')->truncate();
+        $tqData =  TQs::with(['NasExamDetails'=>function($q){
+            $q->select('state_cd','district_cd','udise_sch_code');
+        }])->get();
+        
+        $allGradeCalculateData = array();
+        if(count($tqData)>0)
+        {
+            
+            foreach ($tqData as $key => $tq) 
+            {
+                $questionPassArray = array($tq->tq_q01,$tq->tq_q02,$tq->tq_q03,$tq->tq_q04,$tq->tq_q05,$tq->tq_q06,$tq->tq_q07,$tq->tq_q08,$tq->tq_q09,$tq->tq_q10,$tq->tq_q11,$tq->tq_q12,$tq->tq_q13,$tq->tq_q14,$tq->tq_q15,$tq->tq_q16,$tq->tq_q17,$tq->tq_q18,$tq->tq_q19,$tq->tq_q20,$tq->tq_q21,$tq->tq_q22,$tq->tq_q23,$tq->tq_q24,$tq->tq_q25,$tq->tq_q26,$tq->tq_q27,$tq->tq_q28,$tq->tq_q29,$tq->tq_q30,$tq->tq_q31,$tq->tq_q32,$tq->tq_q33,$tq->tq_q34,$tq->tq_q35,$tq->tq_q36,$tq->tq_q37,$tq->tq_q38,$tq->tq_q39,$tq->tq_q40,$tq->tq_q41,$tq->tq_q42,$tq->tq_q43,$tq->tq_q44,$tq->tq_q45,$tq->tq_q46,$tq->tq_q47,$tq->tq_q48,$tq->tq_q49,$tq->tq_q50,$tq->tq_q51,$tq->tq_q52,$tq->tq_q53,$tq->tq_q54,$tq->tq_q55,$tq->tq_q56,$tq->tq_q57,$tq->tq_q58,$tq->tq_q59,$tq->tq_q60,$tq->tq_q61,$tq->tq_q62,$tq->tq_q63,$tq->tq_q64,$tq->tq_q65,$tq->tq_q66,$tq->tq_q67,$tq->tq_q68,$tq->tq_q69,$tq->tq_q70a,$tq->tq_q70b,$tq->tq_q70c,$tq->tq_q70d,$tq->tq_q71a,$tq->tq_q71b,$tq->tq_q71c,$tq->tq_q71d,$tq->tq_q71e,$tq->tq_q71f);
+
+                $checkLaguage =  $this->checkAnswerfunctiontq($tq->tq_grade,$questionPassArray);
+                $items=array();
+                if(count($checkLaguage[1])>0)
+                {
+                    foreach($checkLaguage[1] as $key=>$value)
+                    {
+                        $items[] = [
+                            'tq_bar'     =>  $tq->tq_bar,
+                            'state_id'     =>  $tq['NasExamDetails']->state_cd,
+                            'district_id'   =>  $tq['NasExamDetails']->district_cd,
+                            'grade'     =>  $tq->tq_grade,
+                            'question_code'=>$key,
+                            'correct_ans'=>($value==1)?$value:0,
+                            'average_performance_in_percentage'=>($value*100)/1,
+                            'created_at' => now(),
+                            'updated_at' => now()
+                        ];
+                        
+                    }
+                }
+                $msgFeed = FeedbackDataTq::insert($items);
+                
+            }
+        }
+
+        $FeedbackData = DB::table('feedback_data_tq')->select('feedback_data_tq.state_id','feedback_data_tq.district_id','feedback_data_tq.grade','feedback_data_tq.question_code','tq_question_master.question_desc  as question',DB::raw("count(feedback_data_tq.id)  AS total_parent"), DB::raw("(SUM(average_performance_in_percentage::float)/count(feedback_data_tq.id)) as avg"))
+        ->leftJoin('tq_question_master','tq_question_master.question_id','=','feedback_data_tq.question_code')
+        ->groupBy('feedback_data_tq.state_id')
+        ->groupBy('feedback_data_tq.district_id')
+        ->groupBy('feedback_data_tq.grade')
+        ->groupBy('feedback_data_tq.question_code')
+        ->groupBy('tq_question_master.question_desc')
+        ->get();
+        $newFeedbackData = array();
+        if(count($FeedbackData)>0)
+        {
+            DB::table('pq_district_level_feedback')->where('level','tq')->truncate();
+            foreach($FeedbackData as $newLOData)
+            {
+                $newFeedbackData['state_id'] = (int)$newLOData->state_id;
+                $newFeedbackData['district_id'] = (int)$newLOData->district_id;
+                $newFeedbackData['grade'] = (int)$newLOData->grade;
+                $newFeedbackData['level']     =  'tq';
+                $newFeedbackData['question_code'] = isset($newLOData->question_code)?$newLOData->question_code:0;
+                $newFeedbackData['question_desc'] = isset($newLOData->question)?$newLOData->question:0;
+                $newFeedbackData['total_parent'] = isset($newLOData->total_parent)?$newLOData->total_parent:0;
+                $newFeedbackData['avg'] = isset($newLOData->avg)?$newLOData->avg:0;
+                $newFeedbackData['created_at'] = now();
+                $newFeedbackData['updated_at'] = now();
+
+                $districtFeedbackFinalData[]=$newFeedbackData;
+            }
+        }
+
+        $dNLOData = PQDistrictLevelFeedback::insert($districtFeedbackFinalData);
+
+        $FeedbackDataForState = DB::table('feedback_data_tq')->select('feedback_data_tq.state_id','feedback_data_tq.grade','feedback_data_tq.question_code','tq_question_master.question_desc  as question',DB::raw("count(feedback_data_tq.id)  AS total_parent"), DB::raw("(SUM(average_performance_in_percentage::float)/count(feedback_data_tq.id)) as avg"))
+        ->leftJoin('tq_question_master','tq_question_master.question_id','=','feedback_data_tq.question_code')
+        ->groupBy('feedback_data_tq.state_id')
+        ->groupBy('feedback_data_tq.grade')
+        ->groupBy('feedback_data_tq.question_code')
+        ->groupBy('tq_question_master.question_desc')
+        ->get();
+        $newFeedbackDataS = array();
+        if(count($FeedbackDataForState)>0)
+        {
+            DB::table('pq_state_level_feedback')->where('level','tq')->truncate();
+            foreach($FeedbackDataForState as $newLODataState)
+            {
+                $newFeedbackDataS['state_id'] = (int)$newLODataState->state_id;
+                $newFeedbackDataS['grade'] = (int)$newLODataState->grade;
+                $newFeedbackData['level']     =  'tq';
+                $newFeedbackDataS['question_code'] = isset($newLODataState->question_code)?$newLODataState->question_code:0;
+                $newFeedbackDataS['question_desc'] = isset($newLODataState->question)?$newLODataState->question:0;
+                $newFeedbackDataS['total_parent'] = isset($newLODataState->total_parent)?$newLODataState->total_parent:0;
+                $newFeedbackDataS['avg'] = isset($newLODataState->avg)?$newLODataState->avg:0;
+                $newFeedbackDataS['created_at'] = now();
+                $newFeedbackDataS['updated_at'] = now();
+
+                $stateFeedbackFinalData[]=$newFeedbackDataS;
+            }
+        }
+
+        $dNLODataState = PQStateLevelFeedback::insert($stateFeedbackFinalData);
+
+        $FeedbackDataNational = DB::table('feedback_data_tq')->select('feedback_data_tq.grade','feedback_data_tq.question_code','tq_question_master.question_desc  as question',DB::raw("count(feedback_data_tq.id)  AS total_parent"), DB::raw("(SUM(average_performance_in_percentage::float)/count(feedback_data_tq.id)) as avg"))
+        ->leftJoin('tq_question_master','tq_question_master.question_id','=','feedback_data_tq.question_code')
+        ->groupBy('feedback_data_tq.grade')
+        ->groupBy('feedback_data_tq.question_code')
+        ->groupBy('tq_question_master.question_desc')
+        ->get();
+        $newFeedbackDataNational = array();
+        if(count($FeedbackDataNational)>0)
+        {
+            DB::table('pq_national_level_feedback')->where('level','tq')->truncate();
+            foreach($FeedbackDataNational as $newLODataNational)
+            {
+                $newFeedbackDataNational['grade'] = (int)$newLODataNational->grade;
+                $newFeedbackData['level']     =  'tq';
+                $newFeedbackDataNational['question_code'] = isset($newLODataNational->question_code)?$newLODataNational->question_code:0;
+                $newFeedbackDataNational['question_desc'] = isset($newLODataNational->question)?$newLODataNational->question:0;
+                $newFeedbackDataNational['total_parent'] = isset($newLODataNational->total_parent)?$newLODataNational->total_parent:0;
+                $newFeedbackDataNational['avg'] = isset($newLODataNational->avg)?$newLODataNational->avg:0;
+                $newFeedbackDataNational['created_at'] = now();
+                $newFeedbackDataNational['updated_at'] = now();
+
+                $NationalFeedbackFinalData[]=$newFeedbackDataNational;
+            }
+        }
+
+        $dNLODataNational = PQNationaLevelFeedback::insert($NationalFeedbackFinalData);
+        if($dNLODataNational)
+        {
+            return "FeedBack Process table successfully created.";
+        }
+        else
+        {
+            return "Something wrong.";
+        }
+    }
+
+    public function checkAnswerfunctiontq($grade,$questionNumbers)
+    {
+         $lagCount = 0 ;
+         $count_subject_wise_total_ans = array();
+         foreach ($questionNumbers as $key => $value) 
+         {
+            //  dd($questionNumbers);
+            $tqQuestionNumber  = $this->excelrowDataTq($key);
+            $checkquestion = TQQuestionMaster::where('grade',ltrim($grade,"0"))->pluck('question_id')->toArray();
+            $result = TqAnswerKey::where('tq_grade',ltrim($grade,"0"))
+                         ->where($tqQuestionNumber,$value)->count();
+            if(in_array($tqQuestionNumber,$checkquestion))
+            {
+                if($result==1)
+                {
+                    @$count_subject_wise_total_ans[$tqQuestionNumber]++;
+                    $lagCount++; 
+                }
+                else
+                {
+                    @$count_subject_wise_total_ans[$tqQuestionNumber]--;
+                }
+            } 
+    
+              
+         }
+         return array($lagCount,$count_subject_wise_total_ans);  
+    }
+
+    public function generateFeedbackanswerkeyTq()
+    {
+       $setData = [3,5,8,10];
+       foreach ($setData as $key => $value) {
+           
+        $tq_key = new TqAnswerKey;
+        $tq_key->tq_grade  = $value;
+        $tq_key->tq_q01  = rand(1,2);
+        $tq_key->tq_q02  = rand(1,2);
+        $tq_key->tq_q03  = rand(1,2);
+        $tq_key->tq_q04  = rand(1,2);
+        $tq_key->tq_q05  = rand(1,2);
+        $tq_key->tq_q06  = rand(1,2);
+        $tq_key->tq_q07  = rand(1,2);
+        $tq_key->tq_q08  = rand(1,2);
+        $tq_key->tq_q09  = rand(1,2);
+        $tq_key->tq_q10  = rand(1,2);
+        $tq_key->tq_q11  = rand(1,2);
+        $tq_key->tq_q12  = rand(1,2);
+        $tq_key->tq_q13  = rand(1,2);
+        $tq_key->tq_q14  = rand(1,2);
+        $tq_key->tq_q15  = rand(1,2);
+        $tq_key->tq_q16  = rand(1,2);
+        $tq_key->tq_q17  = rand(1,2);
+        $tq_key->tq_q18  = rand(1,2);
+        $tq_key->tq_q19  = rand(1,2);
+        $tq_key->tq_q20  = rand(1,2);
+        $tq_key->tq_q21  = rand(1,2);
+        $tq_key->tq_q22  = rand(1,2);
+        $tq_key->tq_q23  = rand(1,2);
+        $tq_key->tq_q24  = rand(1,2);
+        $tq_key->tq_q25  = rand(1,2);
+        $tq_key->tq_q26  = rand(1,2);
+        $tq_key->tq_q27  = rand(1,2);
+        $tq_key->tq_q28  = rand(1,2);
+        $tq_key->tq_q29  = rand(1,2);
+        $tq_key->tq_q30  = rand(1,2);
+        $tq_key->tq_q31  = rand(1,2);
+        $tq_key->tq_q32  = rand(1,2);
+        $tq_key->tq_q33  = rand(1,2);
+        $tq_key->tq_q34  = rand(1,2);
+        $tq_key->tq_q35  = rand(1,2);
+        $tq_key->tq_q36  = rand(1,2);
+        $tq_key->tq_q37  = rand(1,2);
+        $tq_key->tq_q38  = rand(1,2);
+        $tq_key->tq_q39  = rand(1,2);
+        $tq_key->tq_q40  = rand(1,2);
+        $tq_key->tq_q41  = rand(1,2);
+        $tq_key->tq_q42  = rand(1,2);
+        $tq_key->tq_q43  = rand(1,2);
+        $tq_key->tq_q44  = rand(1,2);
+        $tq_key->tq_q45  = rand(1,2);
+        $tq_key->tq_q46  = rand(1,2);
+        $tq_key->tq_q47  = rand(1,2);
+        $tq_key->tq_q48  = rand(1,2);
+        $tq_key->tq_q49  = rand(1,2);
+        $tq_key->tq_q50  = rand(1,2);
+        $tq_key->tq_q51  = rand(1,2);
+        $tq_key->tq_q52  = rand(1,2);
+        $tq_key->tq_q53  = rand(1,2);
+        $tq_key->tq_q54  = rand(1,2);
+        $tq_key->tq_q55  = rand(1,2);
+        $tq_key->tq_q56  = rand(1,2);
+        $tq_key->tq_q57  = rand(1,2);
+        $tq_key->tq_q58  = rand(1,2);
+        $tq_key->tq_q59  = rand(1,2);
+        $tq_key->tq_q60  = rand(1,2);
+        $tq_key->tq_q61  = rand(1,2);
+        $tq_key->tq_q62  = rand(1,2);
+        $tq_key->tq_q63  = rand(1,2);
+        $tq_key->tq_q64  = rand(1,2);
+        $tq_key->tq_q65  = rand(1,2);
+        $tq_key->tq_q66  = rand(1,2);
+        $tq_key->tq_q67  = rand(1,2);
+        $tq_key->tq_q68  = rand(1,2);
+        $tq_key->tq_q69  = rand(1,2);
+        $tq_key->tq_q70a  = rand(1,2);
+        $tq_key->tq_q70b  = rand(1,2);
+        $tq_key->tq_q70a  = rand(1,2);
+        $tq_key->tq_q70c  = rand(1,2);
+        $tq_key->tq_q70d  = rand(1,2);
+        $tq_key->tq_q71a  = rand(1,2);
+        $tq_key->tq_q71b  = rand(1,2);
+        $tq_key->tq_q71c  = rand(1,2);
+        $tq_key->tq_q71d  = rand(1,2);
+        $tq_key->tq_q71e  = rand(1,2);
+        $tq_key->tq_q71f  = rand(1,2);
+        $tq_key->save();
+       }
+
+       return 'Answer Key Generate Succesfully .....'; 
+    }
+
+    function excelrowDataTq($key)
+    {
+      
+       
+           $arr= [
+             'tq_q01'
+            ,'tq_q02' 
+            ,'tq_q03' 
+            ,'tq_q04' 
+            ,'tq_q05' 
+            ,'tq_q06' 
+            ,'tq_q07' 
+            ,'tq_q08' 
+            ,'tq_q09' 
+            ,'tq_q10' 
+            ,'tq_q11' 
+            ,'tq_q12' 
+            ,'tq_q13' 
+            ,'tq_q14' 
+            ,'tq_q15' 
+            ,'tq_q16' 
+            ,'tq_q17' 
+            ,'tq_q18' 
+            ,'tq_q19' 
+            ,'tq_q20' 
+            ,'tq_q21' 
+            ,'tq_q22' 
+            ,'tq_q23' 
+            ,'tq_q24' 
+            ,'tq_q25' 
+            ,'tq_q26' 
+            ,'tq_q27' 
+            ,'tq_q28' 
+            ,'tq_q29' 
+            ,'tq_q30' 
+            ,'tq_q31' 
+            ,'tq_q32' 
+            ,'tq_q33' 
+            ,'tq_q34' 
+            ,'tq_q35' 
+            ,'tq_q36' 
+            ,'tq_q37' 
+            ,'tq_q38' 
+            ,'tq_q39' 
+            ,'tq_q40' 
+            ,'tq_q41' 
+            ,'tq_q42' 
+            ,'tq_q43' 
+            ,'tq_q44' 
+            ,'tq_q45' 
+            ,'tq_q46' 
+            ,'tq_q47' 
+            ,'tq_q48' 
+            ,'tq_q49' 
+            ,'tq_q50' 
+            ,'tq_q51' 
+            ,'tq_q52' 
+            ,'tq_q53' 
+            ,'tq_q54' 
+            ,'tq_q55' 
+            ,'tq_q56' 
+            ,'tq_q57' 
+            ,'tq_q58' 
+            ,'tq_q59' 
+            ,'tq_q60' 
+            ,'tq_q61' 
+            ,'tq_q62' 
+            ,'tq_q63' 
+            ,'tq_q64' 
+            ,'tq_q65' 
+            ,'tq_q66' 
+            ,'tq_q67' 
+            ,'tq_q68' 
+            ,'tq_q69' 
+            ,'tq_q70a' 
+            ,'tq_q70b' 
+            ,'tq_q70c' 
+            ,'tq_q70d' 
+            ,'tq_q71a' 
+            ,'tq_q71b' 
+            ,'tq_q71c' 
+            ,'tq_q71d' 
+            ,'tq_q71e' 
+            ,'tq_q71f'
+            ];
 
         return $arr[$key];
     }
