082218674a09a9f8657588295ca62943606511a6
Mohd umair <mohammad.umair@netprophetsglobal.com>
2022-02-08 13:58:27 +0530

    refactor: webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,15 @@
-const mix = require("laravel-mix");
+const mix = require("laravel-mix")
+const path = require("path")
+const del = require('del')
+
+
 mix.ts("src/index.tsx", "public/js/app.js").react()
-.sass('src/styles/_style.scss', 'public/css');;
+.sass('src/styles/_style.scss', 'public/css')
+
+mix.webpackConfig({
+    resolve: {
+        alias:{
+            '@':path.resolve(__dirname,'src')
+        }
+    }
+});
