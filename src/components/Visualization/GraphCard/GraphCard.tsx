c17221d205f8acb38c2d0e36b6144143f848733e
Akshita <akshita.mittal@netprophetsglobal.com>
2022-02-11 11:57:48 +0530

    Visualizations- highcharts and select dropdown implementation

--- src/components/Visualization/GraphCard/GraphCard.tsx
+++ src/components/Visualization/GraphCard/GraphCard.tsx
@@ -1,6 +1,64 @@
 import React from 'react';
 import People from '@/assets/images/ap-people.svg';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
+const options = {
+    chart: {
+      type: 'column',
+      height: '350'
+    },
+    title: {
+        text: ''
+    },
+    
+    xAxis: {
+        type: 'category'
+    },
+    yAxis: {
+        title: {
+            text: ''
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        column: {
+        pointWidth: 25,
+        },
+        series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y}'
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b>'
+    },
+    series: [
+        {
+            name: "Gender", 
+            colorByPoint: true,
+            data: [
+                {
+                    name: "Boys",
+                    y: 70,
+                    color: "#F2744A"
+                },
+                {
+                    name: "Girls  ",
+                    y: 90,
+                    color: "#F2744A"
+                }
+            ]
+        }
+    ]
+  }
 const GraphCard = () => {
   return (
     <div className="apcard-white">
@@ -18,9 +76,10 @@ const GraphCard = () => {
         </div> 
         <div className="apcard-content">
             <div className="apcard-graph-wrap">
-                <figure className="highcharts-figure">
-                    <div id="c3-gender-langgraph"></div>
-                </figure>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+            />
             </div>  
         </div>    
     </div>
