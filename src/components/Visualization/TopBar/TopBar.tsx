362633381a14049393f763c1cd567ddca5e3b1a9
Akshita <akshita.mittal@netprophetsglobal.com>
2022-02-14 14:03:08 +0530

    Visualizations- dropdown select component

--- src/components/Visualization/TopBar/TopBar.tsx
+++ src/components/Visualization/TopBar/TopBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DropdownSelect from './DropdownSelect';
 
 const TopBar = () => {
   return (
@@ -24,32 +25,7 @@ const TopBar = () => {
                 </button>
             </div>
             <div className="col-md-4">
-            <div className="dropdown-select-wrap">
-                <h3 className="select-label">
-                You can select any state/UT, District or block from here
-                </h3>
-                <div className="dropdown">
-                    <a className="menu-level-main dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside">India</a>
-                    <div className="dropdown-menu menu-level-1">
-                        <div className="inputSearch">
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="dropdown-list">
-                            <a href="#" className="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside">India</a>
-                            <ul className="dropdown-menu menu-level-2">
-                                <li className="dropdown-list">
-                                    <a className="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" href="#"> Andaman & Nicobar Islands</a>
-                                    <ul className="dropdown-menu menu-level-3">
-                                        <li className="dropdown-list"><a className="dropdown-item" href="#"> Nicobar Islands</a></li>
-                                        <li className="dropdown-list"><a className="dropdown-item" href="#"> North and Middle Andaman</a></li>
-                                        <li className="dropdown-list"><a className="dropdown-item" href="#"> South Andaman</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <DropdownSelect />
             </div>
         </div>
     </div>
