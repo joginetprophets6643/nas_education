512be86417947e6a141798be1cee9c67bee976f8
Mohd umair <mohammad.umair@netprophetsglobal.com>
2022-02-24 14:07:09 +0530

    refactor: font-weigjt

--- resources/views/front/report_card/gradesection.blade.php
+++ resources/views/front/report_card/gradesection.blade.php
@@ -15,7 +15,7 @@
                             <li class="nav-item" role="presentation">
                               <button class="nav-link" id="district-tab"  onClick="chageDataWithFilter('global_filter','district')">District</button>
                             </li>
-                            <li class="nav-item" role="presentation"><button class="nav-link"  onClick="javascript:void(0);" id="navbar-highlighter"></button></li>
+                            <li class="nav-item" role="presentation"><button class="nav-link"  onClick="javascript:void(0);" ><b><span id="navbar-highlighter" ></span></b></button></li>
                         </ul>
                         <div class="view-wrap">
                           <!-- <div class="form-check">
