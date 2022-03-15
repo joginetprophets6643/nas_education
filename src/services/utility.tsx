8f06852b7f766d8c1b6e5e59014e8c50563ab1f3
Mohd umair <mohammad.umair@netprophetsglobal.com>
2022-03-15 17:36:50 +0530

    fix changes

--- src/services/utility.tsx
+++ src/services/utility.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://nas21.inroad.in:8055/items/',
+  baseURL: 'http://localhost:8055/items/',
   headers: {
     'Content-type': 'application/json',
     "Authorization": "Bearer $2y$10$Y8lv.M9vBmgHTVTTSrNMHeeXHBE/onZ2en1d0.rjlpyLUPglgX4/2"
