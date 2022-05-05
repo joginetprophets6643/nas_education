1ccaeb848a3d6f9c681bed43283a4f6b32bc203d
sanjay <sanjay.kumar@reseller.netprophetsglobal.com>
2022-05-05 11:11:49 +0530

    device-devices in drc

--- app/Http/Controllers/FinalDistrictProcessController.php
+++ app/Http/Controllers/FinalDistrictProcessController.php
@@ -1913,7 +1913,7 @@ class FinalDistrictProcessController extends Controller
         select id, state_code, dist_code, Dist_pq_q10 as dist_avg, 'pq' level, 'Students go out and play during games period' question_desc, 0 question_code, 0 total_parent
         from grade3districttable
         union all
-        select id, state_code, dist_code, Dist_pq_q08 as dist_avg, 'pq' level, 'Students have access to digital device in the school' question_desc, 0 question_code, 0 total_parent
+        select id, state_code, dist_code, Dist_pq_q08 as dist_avg, 'pq' level, 'Students have access to digital devices in the school' question_desc, 0 question_code, 0 total_parent
         from grade3districttable
         union all
         select id, state_code, dist_code, Dist_pq_q27g as dist_avg, 'pq' level, 'Students have internet connectivity at home' question_desc, 0 question_code, 0 total_parent
@@ -1988,7 +1988,7 @@ class FinalDistrictProcessController extends Controller
         select id, state_code, dist_code, Dist_pq_q10 as dist_avg, 'pq' level, 'Students go out and play during games period' question_desc, 0 question_code, 0 total_parent
         from grade5districttable
         union all
-        select id, state_code, dist_code, Dist_pq_q08 as dist_avg, 'pq' level, 'Students have access to digital device in the school' question_desc, 0 question_code, 0 total_parent
+        select id, state_code, dist_code, Dist_pq_q08 as dist_avg, 'pq' level, 'Students have access to digital devices in the school' question_desc, 0 question_code, 0 total_parent
         from grade5districttable
         union all
         select id, state_code, dist_code, Dist_pq_q27g as dist_avg, 'pq' level, 'Students have internet connectivity at home' question_desc, 0 question_code, 0 total_parent
@@ -2062,7 +2062,7 @@ class FinalDistrictProcessController extends Controller
         select id, state_code, dist_code, Dist_pq_q10 as dist_avg, 'pq' level, 'Students go out and play during games period' question_desc, 0 question_code, 0 total_parent
         from grade8districttable
         union all
-        select id, state_code, dist_code, Dist_pq_q08 as dist_avg, 'pq' level, 'Students have access to digital device in the school' question_desc, 0 question_code, 0 total_parent
+        select id, state_code, dist_code, Dist_pq_q08 as dist_avg, 'pq' level, 'Students have access to digital devices in the school' question_desc, 0 question_code, 0 total_parent
         from grade8districttable
         union all
         select id, state_code, dist_code, Dist_pq_q27g as dist_avg, 'pq' level, 'Students have internet connectivity at home' question_desc, 0 question_code, 0 total_parent
@@ -2136,7 +2136,7 @@ class FinalDistrictProcessController extends Controller
         select id, state_code, dist_code, Dist_pq_q10 as dist_avg, 'pq' level, 'Students go out and play during games period' question_desc, 0 question_code, 0 total_parent
         from grade10districttable
         union all
-        select id, state_code, dist_code, Dist_pq_q08 as dist_avg, 'pq' level, 'Students have access to digital device in the school' question_desc, 0 question_code, 0 total_parent
+        select id, state_code, dist_code, Dist_pq_q08 as dist_avg, 'pq' level, 'Students have access to digital devices in the school' question_desc, 0 question_code, 0 total_parent
         from grade10districttable
         union all
         select id, state_code, dist_code, Dist_pq_q30 as dist_avg, 'pq' level, 'students of class 10 have laboratory facility in school' question_desc, 0 question_code, 0 total_parent
